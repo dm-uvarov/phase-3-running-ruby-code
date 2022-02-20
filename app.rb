990191a0548a6f67edd920184c9e90460c5735f3
Jimmy <dm.a.uvarov@gmail.com>
2022-02-20 16:52:12 -0500

    commit tree 222a79728c72aac2a7b38cc57fef9aa2c4532b97

---
+++ app.rb
@@ -0,0 +1,17 @@
+# app.rb
+
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+
+print "Hello World!"
+print "Hello World!"
+print "Hello World!"
+
+p [1,2,3]
+
+print("Pass this test, please.")
+
+pp "Hello World!"
+pp "Hello World!"
+pp "Hello World!"
